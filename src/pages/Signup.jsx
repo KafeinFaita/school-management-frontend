589d11ceeff6851c4def30da8d526a8cccdb8f3f
paulpdoa <polopdoandres@gmail.com>
2022-02-14 12:53:38 +0800

    try

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -3,32 +3,39 @@ import axios from 'axios';
 
 
 const Signup = () => {
-    const [firstname,setFirstname] = useState('');
-    const [lastname,setLastname] = useState('');
+    const [username,setUsername] = useState('');
+    const [password,setPassword] = useState('');
 
     const onSignup = (e) => {
         e.preventDefault();
-        axios.post('/student',{ fname: firstname, lname: lastname })
+        axios.post('/student',{ username, password })
         .then((data) => {
             console.log(data);
         }).catch(err => console.log(err))
     }
   return (
-    <form onSubmit={onSignup}>
-        <label htmlFor="">First name</label>
-        <input type="text" 
-            value={firstname}  
-            onChange={(e) => setFirstname(e.target.value)}
-        />
+    <div className="flex justify-center items-center h-screen">
+        <div className="max-w-7xl flex flex-col items-center w-full">
+            <h1 className="text-4xl text-gray-800 p-5">Signup</h1>
+            <form onSubmit={onSignup} className="shadow-lg border border-gray-300 p-10 flex flex-col gap-5 items-center w-1/2">
+                <label htmlFor="username">Username</label>
+                <input type="text"
+                    className="border-2 border-gray-300 rounded outline-none p-2 w-1/2" 
+                    value={username}  
+                    onChange={(e) => setUsername(e.target.value)}
+                />
 
-        <label htmlFor="">Last name</label>
-        <input type="text" 
-            value={lastname}  
-            onChange={(e) => setLastname(e.target.value)}
-        />
+                <label htmlFor="password">Password</label>
+                <input type="text"
+                    className="border-2 border-gray-300 rounded outline-none p-2 w-1/2"  
+                    value={password}  
+                    onChange={(e) => setPassword(e.target.value)}
+                />
 
-        <button>Submit</button>
-    </form>
+                <button className="bg-green-500 text-gray-100 rounded p-2">Submit</button>
+            </form>
+        </div>
+    </div>
   );
 };
 
