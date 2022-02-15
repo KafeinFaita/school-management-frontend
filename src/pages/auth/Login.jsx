1b6f447c65ba64b20dfc145d57171e3829c6ec4a
Lynyrd Andres <57025300+KafeinFaita@users.noreply.github.com>
2022-02-15 19:58:18 +0800

    create dashboard layout

--- src/pages/auth/Login.jsx
+++ src/pages/auth/Login.jsx
@@ -1,21 +1,26 @@
 import { useState } from 'react';
 import axios from 'axios';
-
+import { useNavigate } from 'react-router-dom';
 
 const Login = () => {
 
     const [username,setUsername] = useState('');
     const [password,setPassword] = useState('');
 
-    const onSignup = () => {
-        console.log('signup');
+    const navigate = useNavigate();
+
+    const onLogin= () => {
+        axios.post('/login')
+        .then((data) => {
+            navigate(data.data.redirect);
+        })
     }
    
   return (
     <div className="flex justify-center items-center h-screen">
         <div className="max-w-7xl flex flex-col items-center w-full">
             <h1 className="text-4xl text-gray-800 p-5">Login</h1>
-            <form onSubmit={onSignup} className="shadow-lg border border-gray-300 rounded p-10 flex flex-col gap-5 items-center w-1/2">
+            <form onSubmit={onLogin} className="shadow-lg border border-gray-300 rounded p-10 flex flex-col gap-5 items-center w-1/2">
                 <label htmlFor="username">Username</label>
                 <input type="text"
                     className="border-2 border-gray-300 rounded outline-none p-2 w-1/2" 
