6e9f3271a4a9bf80cb00c8ad9e1d901116c9c2fe
paulpdoa <polopdoandres@gmail.com>
2022-02-16 19:02:11 +0800

    still creating pages

--- src/App.js
+++ src/App.js
@@ -1,13 +1,29 @@
 import { Routes,Route } from 'react-router-dom';
+
+import DashboardLayout from './layouts/DashboardLayout';
+
 import Signup from './pages/auth/Signup';
 import Login from './pages/auth/Login';
 
+import Dashboard from './pages/Dashboard';
+import NotFound from './pages/NotFound';
+import Reports from './pages/Reports';
+
+
 function App() {
-  
+
   return (
     <Routes>
       <Route path='/signup' element={ <Signup /> } />
       <Route path='/login' element={ <Login /> } />
+
+      <Route element={ <DashboardLayout /> }>
+        <Route path='/' element={ <Dashboard /> } />
+
+        <Route path='/reports' element={ <Reports /> } />
+      </Route>
+      
+      <Route path="*" element={ <NotFound /> } />
     </Routes>
   );
 }
--- src/pages/auth/Signup.jsx
+++ src/pages/auth/Signup.jsx
@@ -1,11 +1,13 @@
 import { useState } from 'react';
 import axios from 'axios';
-
+import { useNavigate,Link } from 'react-router-dom';
+import { AiOutlineUser,AiOutlineLock } from 'react-icons/ai';
 
 const Signup = () => {
     const [username,setUsername] = useState('');
     const [password,setPassword] = useState('');
 
+    const navigate = useNavigate();
     const onSignup = (e) => {
         e.preventDefault();
         axios.post('/student',{ username, password })
@@ -14,25 +16,28 @@ const Signup = () => {
         }).catch(err => console.log(err))
     }
   return (
-    <div className="flex justify-center items-center h-screen">
-        <div className="max-w-7xl flex flex-col items-center w-full">
-            <h1 className="text-4xl text-gray-800 p-5">Signup</h1>
-            <form onSubmit={onSignup} className="shadow-lg border rounded border-gray-300 p-10 flex flex-col gap-5 items-center w-1/2">
-                <label htmlFor="username">Username</label>
-                <input type="text"
-                    className="border-2 border-gray-300 rounded outline-none p-2 w-1/2" 
-                    value={username}  
-                    onChange={(e) => setUsername(e.target.value)}
-                />
-
-                <label htmlFor="password">Password</label>
-                <input type="text"
-                    className="border-2 border-gray-300 rounded outline-none p-2 w-1/2"  
-                    value={password}  
-                    onChange={(e) => setPassword(e.target.value)}
-                />
-
-                <button className="bg-green-500 text-gray-100 rounded p-2">Submit</button>
+    <div className="flex justify-center h-screen">
+        <div className="max-w-7xl flex items-center justify-center w-full">
+            <form className="bg-gray-100 rounded shadow-lg w-1/3 p-10" onSubmit={onSignup}>
+                <h1 className="font-bold text-4xl text-gray-800 text-center">Signup</h1>
+                <div className="flex flex-col mt-10">
+                    <label className="text-sm text-gray-800" htmlFor="username">Username:</label>
+                    <div className="flex items-center gap-2">
+                        <AiOutlineUser className="text-gray-400" />
+                        <input className="bg-transparent w-full border-b border-gray-500 outline-none p-2" type="text" required />
+                    </div>
+                </div>
+                <div className="flex flex-col mt-10">
+                    <label className="text-sm text-gray-800" htmlFor="username">Password:</label>
+                    <div className="flex gap-2 items-center">
+                        <AiOutlineLock className="text-gray-400" />
+                        <input className="bg-transparent w-full border-b border-gray-500 outline-none p-2" type="password" required />
+                    </div>
+                </div>
+                <div className="flex flex-col gap-5 mt-5">
+                    <button className="p-2 bg-blue-500 rounded text-gray-100">Signup</button>
+                    <Link className="text-sm text-blue-500 text-center" to='/login'>Back to login</Link>
+                </div>
             </form>
         </div>
     </div>
