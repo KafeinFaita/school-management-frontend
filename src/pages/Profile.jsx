39129c52236f16bdea531e7ed2901bad9677e45f
Lynyrd Andres <57025300+KafeinFaita@users.noreply.github.com>
2022-03-02 22:15:56 +0800

    fixed profile

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,33 +1,12 @@
 import Datetime from "../components/Datetime"
-import { useContext,useEffect } from 'react';
-import { Navigate } from 'react-router-dom';
-import { GlobalContext } from "../helper/Context";
-import { fetchData } from "../helper/function";
+
 
 const Profile = () => {
-
- const { isVerified,setIsVerified,setIsAuth,setErrMssg,setIsAuthenticated,isAuthenticated } = useContext(GlobalContext);
-
- useEffect(() => {
-   const abortCont = new AbortController();
-
-   fetchData({ signal:abortCont.signal },setIsVerified,setIsAuth,setErrMssg,setIsAuthenticated);
-   
-   return () => abortCont.abort();
- }, [isVerified,isAuthenticated]);
-
- console.log(isVerified,isAuthenticated)
-
- if(isVerified === true && isAuthenticated === false || isVerified === true && isAuthenticated === true ) {
-    return (
-      <div className="px-10 h-full">
-        <Datetime title="Profile" />
-      </div>
-    )
- }
-
-  
-  return <Navigate to='/login' />
+  return (
+    <div className="px-10 h-full">
+      <Datetime title="Profile" />
+    </div>
+  )
 }
 
 export default Profile
