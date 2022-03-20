030552c4e36851e06936d522939a13d2d0f0603f
paulpdoa <polopdoandres@gmail.com>
2022-03-20 18:56:16 +0800

    done with modal user

--- src/components/users/UserModal.jsx
+++ src/components/users/UserModal.jsx
@@ -1,17 +1,22 @@
 import { baseUrl } from "../../helper/function";
+import { useState } from 'react';
 import axios from "axios";
 
 const UserModal = ({setApprove,userDetail}) => {  
 
+    const [isLoading,setIsLoading] = useState(true);
+
     const verifyUser = async (id,verify) => {
         const checkUser = !verify;
+        setIsLoading(!isLoading);
         try {
-            const data = await axios.patch(`${baseUrl()}user/${id}`,{ id, checkUser });
-            console.log(data);
+            const data = await axios.patch(`${baseUrl()}users/${id}`,{ id, checkUser });
+            setIsLoading(!isLoading);
+            setApprove(false);
           }
-          catch(err) {
-            console.log(err);
-          }
+        catch(err) {
+          console.log(err);
+        }
       } 
 
   return (
@@ -19,7 +24,7 @@ const UserModal = ({setApprove,userDetail}) => {
         <div className="bg-gray-100 p-20 shadow-lg rounded-md z-50">
             <p className="font-semibold text-xl">Do you want to verify { userDetail.username }?</p>
             <div className="flex justify-center gap-2 p-2">
-                <button onClick={() => verifyUser(userDetail._id,userDetail.isVerified)} className="bg-green-500 text-gray-100 p-2 rounded">Verify</button>
+                <button onClick={() => verifyUser(userDetail._id,userDetail.isVerified)} className="bg-green-500 text-gray-100 p-2 rounded">{!isLoading ? 'Verifying' : 'Yes'}</button>
                 <button className="bg-red-500 text-gray-100 p-2 rounded" onClick={() => setApprove(false)}>Close</button>
             </div>
         </div>
