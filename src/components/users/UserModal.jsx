fb9fecb441bae91585299066799f56c85ce216ef
Lynyrd Andres <57025300+KafeinFaita@users.noreply.github.com>
2022-03-20 19:50:12 +0800

    Asd

--- src/components/users/UserModal.jsx
+++ src/components/users/UserModal.jsx
@@ -6,7 +6,7 @@ const UserModal = ({setApprove,userDetail}) => {
     const verifyUser = async (id,verify) => {
         const checkUser = !verify;
         try {
-            const data = await axios.patch(`${baseUrl()}user/${id}`,{ id, checkUser });
+            const data = await axios.patch(`${baseUrl()}users/${id}`,{ id, checkUser });
             console.log(data);
           }
           catch(err) {
@@ -17,7 +17,7 @@ const UserModal = ({setApprove,userDetail}) => {
   return (
     <div className="absolute top-0 h-4/5 w-full left-0 flex items-center justify-center">
         <div className="bg-gray-100 p-20 shadow-lg rounded-md z-50">
-            <p className="font-semibold text-xl">Do you want to verify { userDetail.username }?</p>
+            <p className="font-semibold text-xl">Do you want to { userDetail.isVerified ? 'unverify' : 'verify' } { userDetail.username }?</p>
             <div className="flex justify-center gap-2 p-2">
                 <button onClick={() => verifyUser(userDetail._id,userDetail.isVerified)} className="bg-green-500 text-gray-100 p-2 rounded">Verify</button>
                 <button className="bg-red-500 text-gray-100 p-2 rounded" onClick={() => setApprove(false)}>Close</button>
